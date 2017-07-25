e2b59598a936b3e4722e09f64e20ea27072712a5
Thomas Bachem <mail@thomasbachem.com>
2017-07-25 12:29:08 +0200

    Do not include empty smooch-public.css file

--- public/class-smooch-public.php
+++ public/class-smooch-public.php
@@ -60,21 +60,7 @@ class Smooch_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Smooch_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Smooch_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/smooch-public.css', array(), $this->version, 'all' );
-
+		
 	}
 
 	/**
