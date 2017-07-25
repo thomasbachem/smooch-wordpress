67dd9f04b951b5d59ea9ae54ff7b3b98ba26be58
Thomas Bachem <mail@thomasbachem.com>
2017-07-25 12:30:41 +0200

    Remove JS file stub

--- public/class-smooch-public.php
+++ public/class-smooch-public.php
@@ -83,18 +83,7 @@ class Smooch_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Smooch_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Smooch_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+		
 	}
 
 	/**
